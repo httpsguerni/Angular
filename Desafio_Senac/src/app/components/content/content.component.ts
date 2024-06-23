366380c081f50e52401e3279352d542bcdb60e55
Saiury <saiury@saiury>
2024-06-23 15:34:57 -0300

    adição de typescript ao código ❤️

--- Desafio_Senac/src/app/components/content/content.component.ts
+++ Desafio_Senac/src/app/components/content/content.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
+
+  education={
+    dreams:'Vários sonhos, o mesmo caminho: a educação!',
+    since:'Desde 1946, trabalhamos para transformar vidas. Aqui, pessoas de todas as idades são incentivadas a desenvolver seu potencial para expandir suas possibilidades de futuro. É assim que formamos profissionais que sabem fazer grandes ideias virarem realidade.'
+  }
 
 }
--- Desafio_Senac/src/app/components/footer/footer.component.ts
+++ Desafio_Senac/src/app/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  curso={ 
+    senac: 'O Senac',
+    cursos:'Cursos',
+    unidades:'Unidades'
+  }
+
+  copyright={
+    copy:'Copyright © 2020. Todos os Direitos Reservados',
+    senac:'Serviço Nacional de Aprendizagem Comercial - SP',
+    cnpj:'CNPJ: 03.709.814/0001-98'
+
+  }
+   
+
+  
 }
